dfc31dabc5d9e1a3b9b2e33fd97ab9c5b6645dd9
Zeph Lafassett <zeph@lafassett.com>
2014-04-24 10:32:15 -0700

    junk checkin, nothing good works

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ var options = {
 crawler = new Crawler(options);
 console.dir(crawler);
 crawler.init();
+crawler.setUrl("http://allrecipes.com/Recipe/Almond-Lemon-Chicken/Detail.aspx");
 console.log(crawler.url);
-crawler.setUrl("asdf");
-console.log(crawler.url);
+
+crawler.on("fetch:success", function (res) {
+	console.log(res);
+});
+
+crawler.on("fetch:error", function (error) {
+	console.log(error);
+});
+
+crawler.fetch();
