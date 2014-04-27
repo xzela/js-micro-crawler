9045e5990e1cf066d13c57a56546b0134cd1bbd6
Zeph Lafassett <zeph@lafassett.com>
2014-04-27 09:19:14 -0700

    parsing the ingredients

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-var Crawler = require('./lib/crawler');
+var Crawler = require('./lib/crawler'),
+	htmlparser = require('htmlparser2'),
+	cheerio = require('cheerio'),
+	fs = require('fs');
 
 var options = {
 
 };
 
-crawler = new Crawler(options);
-console.dir(crawler);
-crawler.init();
-crawler.setUrl("http://allrecipes.com/Recipe/Almond-Lemon-Chicken/Detail.aspx");
-console.log(crawler.url);
 
-crawler.on("fetch:success", function (res) {
-	console.log(res);
+fs.readFile('./data/test.html', {encoding: 'utf8'}, function (err, data) {
+	if (err) {
+		throw err;
+	}
+	var $ = cheerio.load(data);
+	var junk = $('#liIngredient').each(function (idx, elem) {
+		console.log($(this).children().find('p').find('span').text());
+	});
+
 });
 
-crawler.on("fetch:error", function (error) {
-	console.log(error);
-});
+// crawler = new Crawler(options);
+// console.dir(crawler);
+// crawler.init();
+// crawler.setUrl("http://allrecipes.com/Recipe/Almond-Lemon-Chicken/Detail.aspx");
+// console.log(crawler.url);
 
-crawler.fetch();
+// crawler.on("fetch:success", function (res) {
+// 	console.log(res);
+// });
+
+// crawler.on("fetch:error", function (error) {
+// 	console.log(error);
+// });
+
+// crwaler.on('parse:success', function (data) {
+
+// });
+
+// crawler.fetch();
