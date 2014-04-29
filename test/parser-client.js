81426a0aeb612206d42f02f5d538cb162007d8cb
Zeph Lafassett <zeph@lafassett.com>
2014-04-29 09:18:11 -0700

    Adding true object tests

--- test/parser-client.js
+++ test/parser-client.js
@@ -4,9 +4,24 @@ var should = require('should'),
 describe('Parser Client Unit Tests - Client', function () {
 
 	describe('Initializing the Parser', function () {
-		it('should fail if no options are supplied', function (done) {
+		it('should fail if no schema is supplied', function (done) {
 			(function () {
 				new MicroParser();
+			}).should.throw();
+			done();
+		});
+		it('should fail if the schema is not an Object', function (done) {
+			(function () {
+				new MicroParser('');
+			}).should.throw();
+			(function () {
+				new MicroParser(1);
+			}).should.throw();
+			(function () {
+				new MicroParser([]);
+			}).should.throw();
+			(function () {
+				new MicroParser(false);
 			}).should.throw();
 			done();
 		});
